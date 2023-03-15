bef247e37f4bec765ca4cb7f240167fd89a08538
Mariana Soares <81964220+Mariana-gs@users.noreply.github.com>
2023-03-15 15:19:00 -0300

    Creating Project & Character

---
+++ Source/ActionRoguelikeIM/ActionRoguelikeIMGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRoguelikeIMGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKEIM_API AActionRoguelikeIMGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
